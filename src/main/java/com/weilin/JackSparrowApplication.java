d5288125347eae4e6b04be619e0abffc1bc5d4a4
weilin-0821 <2976647106weilin@gmail.com>
2026-03-02 23:54:48 +0800

    Initial commit

---
+++ src/main/java/com/weilin/JackSparrowApplication.java
@@ -0,0 +1,13 @@
+package com.weilin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackSparrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackSparrowApplication.class, args);
+    }
+
+}
