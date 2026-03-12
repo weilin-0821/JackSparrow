6e7ef24c7d4c57c49aa11c70bf71160fe4f0349b
weilin-0821 <2976647106weilin@gmail.com>
2026-03-12 23:38:59 +0800

    redis和elasticSearch的提供者加入枚举类

--- src/main/java/com/weilin/datasource/provider/ElasticSearchProvider.java
+++ src/main/java/com/weilin/datasource/provider/ElasticSearchProvider.java
@@ -3,6 +3,7 @@ package com.weilin.datasource.provider;
 import com.alibaba.fastjson.JSONObject;
 import com.weilin.annotation.MiddlewareType;
 import com.weilin.datasource.DataSourceProvider;
+import com.weilin.enums.ConfigKeyEnum;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
@@ -15,9 +16,9 @@ public class ElasticSearchProvider implements DataSourceProvider {
     @Override
     public Object create(JSONObject config) {
         RestClientBuilder builder = RestClient.builder(
-                new HttpHost(config.getString("host"),
-                        config.getInteger("port"),
-                        config.getString("scheme")
+                new HttpHost(config.getString(ConfigKeyEnum.HOST.getKey()),
+                        config.getInteger(ConfigKeyEnum.PORT.getKey()),
+                        config.getString(ConfigKeyEnum.SCHEME.getKey())
                 )
         );
         RestHighLevelClient client = new RestHighLevelClient(builder);
--- src/main/java/com/weilin/datasource/provider/RedisProvider.java
+++ src/main/java/com/weilin/datasource/provider/RedisProvider.java
@@ -16,24 +16,24 @@ public class RedisProvider implements DataSourceProvider {
 
     @Override
     public Object create(JSONObject config) {
-        JSONObject params = config.getJSONObject("params");
+        JSONObject params = config.getJSONObject(ConfigKeyEnum.PARAMS.getKey());
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(params.getInteger(ConfigKeyEnum.MAX_TOTAL.getKey()));
         poolConfig.setMaxIdle(params.getInteger(ConfigKeyEnum.MAX_IDLE.getKey()));
         poolConfig.setMinIdle(params.getInteger(ConfigKeyEnum.MIN_IDLE.getKey()));
         // 如果有密码，在创建JedisPool时传入
-        if(Objects.equals(config.getString("password"), "")) {
+        if(Objects.equals(config.getString(ConfigKeyEnum.PASSWORD.getKey()), "")) {
             return new JedisPool(poolConfig,
-                    config.getString("host"),
-                    config.getInteger("port"),
-                    params.getInteger("timeOut")
+                    config.getString(ConfigKeyEnum.HOST.getKey()),
+                    config.getInteger(ConfigKeyEnum.PORT.getKey()),
+                    params.getInteger(ConfigKeyEnum.TIMEOUT.getKey())
             );
         }else {
             return new JedisPool(poolConfig,
-                    config.getString("host"),
-                    config.getInteger("port"),
-                    params.getInteger("timeOut"),
-                    config.getString("password")
+                    config.getString(ConfigKeyEnum.HOST.getKey()),
+                    config.getInteger(ConfigKeyEnum.PORT.getKey()),
+                    params.getInteger(ConfigKeyEnum.TIMEOUT.getKey()),
+                    config.getString(ConfigKeyEnum.PASSWORD.getKey())
             );
         }   }
 }
